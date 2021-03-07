a01341bc27db46b46ba7e35760be5a1c4e183d38
NelsonWang <wangnelson2@gmail.com>
2021-03-07 04:43:36 -0500

    recursion | Fibo Basic + Fibo Sequence with array?

--- algorithms/anatomyOfrecursion.js
+++ algorithms/anatomyOfrecursion.js
@@ -12,7 +12,39 @@ inception();
 
 
 
-const factorial = (n) =>  (n <= 1) ? n : (n * factorial(n - 1));
-
-
+const factorial = (n) => (n <= 1) ? n : (n * factorial(n - 1));
 factorial(5);
+
+var a = 1,b = 0, counter = 0;
+
+var fiboSecuence = [0,1]
+
+function fibonacciArr(n){
+    let sequenceLen = fiboSecuence.length;
+    let c = fiboSecuence[sequenceLen-1] + fiboSecuence[sequenceLen-2];
+    fiboSecuence.push(c);
+    // console.log(c);
+    if(c >= n){
+        //I mean it works as long that you not asking the 0 and 1s though
+        return sequenceLen;
+    }
+    return fibonacciArr(n);
+}
+
+fibonacciArr(144);
+
+
+
+function FibonacciBasic(n) {
+    let c = a+b;
+    if(c >= n){
+        return counter
+    }
+    counter++;
+    a = b;
+    b = c;
+    return Fibonacci(n); 
+
+}
+
+Fibonacci(3)
