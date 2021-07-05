ac71e92ab945da952a61d101f0dc65affbc10f43
NelsonWang <wangnelson2@gmail.com>
2021-07-05 14:53:35 -0400

    typescript-sandbox | testing | atuples

--- Typescript/tssandbox/types.ts
+++ Typescript/tssandbox/types.ts
@@ -4,15 +4,20 @@ let myBool: boolean;
 
 let strArr: string[];
 let numArr: number[];
+let boolArr: Array<boolean>;
+
+let strNumTuple: [string, number]
 
 myString = "Hello "+" "+"World".slice(0,3);
 myNum = 2.2;
 myBool = false;
 
 
-
 strArr = ["Hello", "World"];
 numArr = [1,2,3];
+boolArr = [true, false, true];
+
+strNumTuple = ['hello', 4,];
 
 
 console.log(myString);
