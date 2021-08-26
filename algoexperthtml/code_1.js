0f83f9d74a487d100d248fb25111686682221054
NelsonWang <wangnelson2@gmail.com>
2021-08-25 23:45:07 -0400

    algo | 3 | max function

--- algoexperthtml/code_1.js
+++ algoexperthtml/code_1.js
@@ -1,5 +1,3 @@
-
-
 competition = [
     ["HTML", "C#"],
     ["C#", "Python"],
@@ -11,18 +9,35 @@ results = [0, 0, 1]
 
 console.log(tournamentWinner(competition, results));
 
-function tournamentWinner(competitions, results) {
-    
-    const scores = {'':0};
 
-    competitions.forEach(competition => {
+function tournamentWinner(competitions, results) {
+
+    const scores = {
+        '': 0
+    };
+    const length = results.length;
+    for (i = 0; i < length; i++) {
+        const competition = competitions[i];
         const [homeTeam, awayTeam] = competition;
-        
+
+        addIfExists(scores, results[i] == 1 ? homeTeam : awayTeam);
+        // addIfExists(scores, awayTeam);
 
         console.log(homeTeam);
         console.log(awayTeam);
-    });
-    
+        console.log(scores)
+    };
 
-    return '';
-  }
+    // Get map with longest mapped item
+    const getMax = (a, b) => Math.max(a, b);
+    return Object.keys(results).reduce(getMax, -Infinity);
+}
+
+function addIfExists(score, winningTeam) {
+    if (score[winningTeam] != null) {
+        score[winningTeam] += 1;
+    } else {
+        score[winningTeam] = 1;
+    }
+    return score;
+}
