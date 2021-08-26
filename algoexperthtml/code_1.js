eef151c03111bb4a676c595d6e58c23d5ffa96a6
NelsonWang <wangnelson2@gmail.com>
2021-08-26 00:11:09 -0400

    algo | 3 | fix code?

--- algoexperthtml/code_1.js
+++ algoexperthtml/code_1.js
@@ -4,8 +4,20 @@ competition = [
     ["Python", "HTML"],
 
 ]
+// competition = [
+//     ["HTML", "C#"],
+//     ["C#", "Python"],
+//     ["Python", "HTML"],
 
-results = [0, 0, 1]
+// ]
+
+competition = [
+    ["HTML", "Java"],
+    ["Java", "Python"],
+    ["Python", "HTML"]
+  ]
+
+results = [0, 1, 1]
 
 console.log(tournamentWinner(competition, results));
 
@@ -23,14 +35,18 @@ function tournamentWinner(competitions, results) {
         addIfExists(scores, results[i] == 1 ? homeTeam : awayTeam);
         // addIfExists(scores, awayTeam);
 
-        console.log(homeTeam);
-        console.log(awayTeam);
-        console.log(scores)
+        // console.log(homeTeam);
+        // console.log(awayTeam);
+        // console.log(scores)
     };
 
-    // Get map with longest mapped item
-    const getMax = (a, b) => Math.max(a, b);
-    return Object.keys(results).reduce(getMax, -Infinity);
+    const getMax = (a, b) => {
+        return Math.max(a, b);
+    };
+    const getKeyByValue = (object, value) => {
+        return Object.keys(object).find(key => object[key] === value)
+    }
+    return getKeyByValue(scores, Object.values(scores).reduce(getMax, -Infinity));
 }
 
 function addIfExists(score, winningTeam) {
