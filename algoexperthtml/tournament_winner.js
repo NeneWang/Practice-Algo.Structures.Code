8f162bcc66e4d0ebebd2627d983bc63837b438b4
NelsonWang <wangnelson2@gmail.com>
2021-08-26 17:31:15 -0400

    algo | 4| solution

--- algoexperthtml/tournament_winner.js
+++ algoexperthtml/tournament_winner.js
@@ -1,1 +1,18 @@
 coins = [5,7,1,1,2,3,22]
+
+console.log(nonConstructibleChange(coins));
+
+function nonConstructibleChange(coins) {
+    // Write your code here.
+    var currentChange = 0;
+    coins.sort((a,b) => a-b)
+    coins.forEach(coin => {
+        // console.log(coin);
+        
+        if(coin>currentChange+1){
+            return currentChange+1
+        }
+        currentChange+=coin;
+    })
+    return currentChange+1;
+  }
