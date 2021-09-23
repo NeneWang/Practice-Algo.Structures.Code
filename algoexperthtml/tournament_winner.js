a67026deed99a43d78fac0fa399a0ab8381e915a
NelsonWang <wangnelson2@gmail.com>
2021-09-23 14:07:22 -0400

    algo | findclosest BST

--- algoexperthtml/tournament_winner.js
+++ algoexperthtml/tournament_winner.js
@@ -1,18 +1,46 @@
-coins = [5,7,1,1,2,3,22]
+coins = [5, 7, 1, 1, 2, 3, 22]
 
 console.log(nonConstructibleChange(coins));
 
 function nonConstructibleChange(coins) {
     // Write your code here.
     var currentChange = 0;
-    coins.sort((a,b) => a-b)
+    coins.sort((a, b) => a - b)
     coins.forEach(coin => {
         // console.log(coin);
-        
-        if(coin>currentChange+1){
-            return currentChange+1
+
+        if (coin > currentChange + 1) {
+            return currentChange + 1
         }
-        currentChange+=coin;
+        currentChange += coin;
     })
-    return currentChange+1;
-  }
+    return currentChange + 1;
+}
+
+function findClosestValueInBst(tree, target) {
+    // Write your code here.
+}
+
+function findClosestValueInBsHelper(tree, target, closest) {
+    if (tree = null) return closest;
+    if (Math.abs(target - closest) > Math.abs(target - tree.value)) {
+        closest = tree.value; 
+        // My guess that this line of code makes the closest to this, if it is indeed closer than the one before
+    }
+    // Then you have to traverse the code in order to have the tree traversal
+    if(target < tree.value){
+        return findClosestValueInBsHelper(tree.left, target, clsoest);
+    }
+    if(target > tree.value){
+        return findClosestValueInBsHelper(tree.right, target. closest);
+    }
+}
+
+// This is the class of the input tree. Do not edit.
+class BST {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
