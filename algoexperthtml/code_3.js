77e2cf3a3415a013a69729b29b10922f2558bc53
NelsonWang <wangnelson2@gmail.com>
2021-09-25 18:21:46 -0400

    algo | node Depth | 3

--- algoexperthtml/code_3.js
+++ algoexperthtml/code_3.js
@@ -33,3 +33,19 @@ class BST {
         this.right = null;
     }
 }
+
+function nodeDepths(root, depth = 0) {
+    // Write your code here.
+    if (root === null) return 0;
+    return depth + nodeDepths(root.right, 1) + nodeDepths(root.left, depth + 1);
+
+}
+
+// This is the class of the input binary tree.
+class BinaryTree {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
