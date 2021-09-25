a16fff5eb73c3076ea9de94d71647628ba663c14
NelsonWang <wangnelson2@gmail.com>
2021-09-25 07:04:17 -0400

    algo | branch sums

--- algoexperthtml/code_1.js
+++ algoexperthtml/code_1.js
@@ -15,7 +15,7 @@ competition = [
     ["HTML", "Java"],
     ["Java", "Python"],
     ["Python", "HTML"]
-  ]
+]
 
 results = [0, 1, 1]
 
@@ -56,7 +56,20 @@ function addIfExists(score, winningTeam) {
 
 function branchSums(root) {
     // Write your code here.
-  }
-  
+    const sums = [];
+    calculateBranchSums(root, 0, sums);
+    return sums;
+}
 
+function calculateBranchSums(node, runningSum, sums) {
+    if (!node) return;
+    const newRunningSum = runningSum + node.value;
+    if (!node.left && !node.right) {
+        sums.push(newRunningSum);
+        return;
+    }
 
+    calculateBranchSums(node.left, newRunningSum, sums);
+    calculateBranchSums(node.right, newRunningSum, sums);
+
+}
