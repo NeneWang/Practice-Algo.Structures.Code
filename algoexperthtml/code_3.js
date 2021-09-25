e50569814d266d0684c39786ccc4c1ffd2073b48
NelsonWang <wangnelson2@gmail.com>
2021-09-25 15:20:01 -0400

    algo | closest | 3

--- algoexperthtml/code_3.js
+++ algoexperthtml/code_3.js
@@ -1,1 +1,35 @@
 // I am going to rewrite all my code here? Or no need since I will revisit here with a different language?
+
+
+
+function findClosestValueInBst(tree, target) {
+    // Write your code here.
+    return findClosestValueRecursive(tree, target, tree.value);
+}
+
+function findClosestValueRecursive(node, target, closestTarget) {
+
+    if (node === null) return closestTarget;
+    if (Math.abs(node.value - target) < Math.abs(closestTarget - target)) {
+        closestTarget = node.value;
+    }
+
+    if (target > node.value) {
+        return findClosestValueRecursive(node.right, target, closestTarget);
+    } else if (target < node.value) {
+        return findClosestValueRecursive(node.right, target, closestTarget);
+    } else {
+        return node.value;
+    }
+
+
+}
+
+// This is the class of the input tree. Do not edit.
+class BST {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
