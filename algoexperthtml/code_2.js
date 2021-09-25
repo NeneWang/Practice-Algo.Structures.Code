1b9adaafe31c122fd8f316fc20759cb1b201f847
NelsonWang <wangnelson2@gmail.com>
2021-09-25 18:18:28 -0400

    algo | node Depth | rec 2

--- algoexperthtml/code_2.js
+++ algoexperthtml/code_2.js
@@ -67,3 +67,18 @@ function branchSumRecursive(node, currentSum, sums) {
     branchSumRecursive(node.left, newCurrentSum, sums);
     branchSumRecursive(node.right, newCurrentSum, sums);
 }
+
+function nodeDepths(root, depth = 0) {
+    // Write your code here.
+    if (root === null) return 0;
+    return depth + nodeDepths(root.right, depth + 1) + nodeDepths(root.left, depth + 1);
+}
+
+// This is the class of the input binary tree.
+class BinaryTree {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
