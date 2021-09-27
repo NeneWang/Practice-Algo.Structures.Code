eeff922bfec39ecda7983037f246955e40e31ec1
NelsonWang <wangnelson2@gmail.com>
2021-09-26 21:56:17 -0400

    depth_first | 1

--- algoexperthtml/code_1.js
+++ algoexperthtml/code_1.js
@@ -148,3 +148,20 @@ class BinaryTree {
         this.right = null;
     }
 }
+
+
+class Node {
+    constructor(name) {
+      this.name = name;
+      this.children = [];
+    }
+  
+    addChild(name) {
+      this.children.push(new Node(name));
+      return this;
+    }
+  
+    depthFirstSearch(array) {
+      // Write your code here.
+    }
+  }
