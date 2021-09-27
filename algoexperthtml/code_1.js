63cf80891c3b9ed4c78e5680203650df94f40967
NelsonWang <wangnelson2@gmail.com>
2021-09-27 12:18:14 -0400

    algo | class photos | 1

--- algoexperthtml/code_1.js
+++ algoexperthtml/code_1.js
@@ -184,8 +184,29 @@ function minimumWaitingTime(queries) {
     totalTime = 0;
     const queryLen = queries.length;
     for (let i = 0; i < queryLen - 1; i++) {
-        totalTime += (queryLen-i-1) * queries[i];
+        totalTime += (queryLen - i - 1) * queries[i];
     }
     console.log(totalTime);
 
+}
+
+
+function classPhotos(redShirtHeights, blueShirtHeights) {
+    // Write your code here.
+    redShirtHeights.sort((a, b) => a - b);
+    blueShirtHeights.sort((a, b) => a - b);
+
+    const shirtColorInFirstRow = redShirtHeights[0] < blueShirtHeights[0] ? 'RED' : 'BLUE';
+    for (let idx = 0; idx < redShirtHeights.length; idx++) {
+        const redShirtHeight = redShirtHeights[idx];
+        const blueShirtHeight = blueShirtHeights[idx];
+
+        if (shirtColorInFirstRow === 'RED') {
+            if (redShirtHeights >= blueShirtHeight) return false;
+        } else if (blueShirtHeight >= redShirtHeight) return false;
+
+    }
+
+
+    return false;
 }
