729d1753ae02d5b653060c72bc9c2a0461595ca5
NelsonWang <wangnelson2@gmail.com>
2021-09-28 07:02:48 -0400

    algo | class photos | truefalse

--- algoexperthtml/code_1.js
+++ algoexperthtml/code_1.js
@@ -196,17 +196,15 @@ function classPhotos(redShirtHeights, blueShirtHeights) {
     redShirtHeights.sort((a, b) => b - a);
     blueShirtHeights.sort((a, b) => b - a);
 
-    const shirtColorInFirstRow = redShirtHeights[0] < blueShirtHeights[0] ? 'RED' : 'BLUE';
+    const shirtColorInFirstRowRed = redShirtHeights[0] < blueShirtHeights[0] ? true : false;
     for (let idx = 0; idx < redShirtHeights.length; idx++) {
         const redShirtHeight = redShirtHeights[idx];
         const blueShirtHeight = blueShirtHeights[idx];
 
-        if (shirtColorInFirstRow === 'RED') {
-            if (redShirtHeights >= blueShirtHeight) return false;
+        if (shirtColorInFirstRowRed) {
+            if (redShirtHeight >= blueShirtHeight) return false;
         } else if (blueShirtHeight >= redShirtHeight) return false;
 
     }
-
-
     return true;
 }
