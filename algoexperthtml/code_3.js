9a203e2165d75e5d437e5541f7332bfb5ffca0e9
NelsonWang <wangnelson2@gmail.com>
2021-09-28 14:26:17 -0400

    algo | remove duplicated linked list | code_3

--- algoexperthtml/code_3.js
+++ algoexperthtml/code_3.js
@@ -72,3 +72,26 @@ class BinaryTree {
         this.right = null;
     }
 }
+
+// This is an input class. Do not edit.
+class LinkedList {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+function removeDuplicatesFromLinkedList(linkedList) {
+    // Write your code here.
+    let currentNode = linkedList;
+    while (currentNode !== null) {
+        let nextNode = currentNode.next;
+        while (nextNode !== null && nextNode.value === currentNode.value) {
+            nextNode = nextNode.next;
+        }
+        currentNode.next = nextNode;
+        currentNode = nextNode;
+    }
+
+    return linkedList;
+}
