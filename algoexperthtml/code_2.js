addfa49e534daec8597e35525c4a02208a5b5392
NelsonWang <wangnelson2@gmail.com>
2021-09-28 14:21:13 -0400

    algo | remove duplicated linked list | currentvalue

--- algoexperthtml/code_2.js
+++ algoexperthtml/code_2.js
@@ -82,3 +82,24 @@ class BinaryTree {
         this.right = null;
     }
 }
+
+
+class LinkedList {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+function removeDuplicatesFromLinkedList(linkedList) {
+    let currentNode = linkedList;
+    while (currentNode !== null) {
+        let nextNode = currentNode.next;
+        while(nextNode !== null && nextNode.value === currentNode.value ){
+            nextNode = nextNode.next;
+        }
+        currentNode.next = nextNode;
+        currentNode = nextNode;
+    }
+    return linkedList;
+}
