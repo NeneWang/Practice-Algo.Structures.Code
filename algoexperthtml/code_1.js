87c489332378b947aa1d1994facfcc4de756a772
NelsonWang <wangnelson2@gmail.com>
2021-09-30 13:59:26 -0400

    algo | product sum |

--- algoexperthtml/code_1.js
+++ algoexperthtml/code_1.js
@@ -295,3 +295,17 @@ function getNthFibMemoize(n, memoize = {
         return getNthFibMemoize(n - 1, memoize) + getNthFibMemoize(n - 2, memoize);
     }
 }
+
+
+function productSum(array, multiplier = 1) {
+    // Write your code here.
+    let sum = 0;
+    for (const element of array) {
+        if (Array.isArray(element)) {
+            sum += productSum(element, multiplier + 1);
+        } else {
+            sum += element;
+        }
+    }
+    return sum * multiplier;
+}
