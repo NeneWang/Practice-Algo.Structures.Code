d301e29bd7ec35345e394b597b3936960f05f0c3
NelsonWang <wangnelson2@gmail.com>
2021-10-01 10:15:42 -0400

    algo | findThreeLargest | 4

--- algoexperthtml/code_3.js
+++ algoexperthtml/code_3.js
@@ -165,32 +165,10 @@ function binarySearchHelper2(array, target, left, right) {
 
 
 function findThreeLargestNumbers(array) {
-    // Write your code here.
-    const threeLargest = [null, null, null];
-    for (const num of array) {
-        updatedLargest(threeLargest, num);
-    }
-    return threeLargest;
 }
 
-function updateLargest(threeLargest, num) {
-    if (threeLargest[2] === null || num > threeLargest[2]) {
-        shiftAndUpdate(array, num, 2);
-    } else if (threeLargest[1] === null || num > threeLargest[1]) {
-        shiftAndUpdate(array, num, 1);
-    } else if (threeLargest[0] === null || num > threeLargest[0]) {
-        shiftAndUpdate(array, num, 0);
-    }
-
-
+function updatedLargest(threeLargest, num) {
 }
 
 function shiftAndUpdate(array, num, idx) {
-    for (let i = 0; i < idx; i++) {
-        if(i == idx){
-            array[i] = num;
-        }else{
-            array[i] = array[i+1];
-        }
-    }
 }
--- algoexperthtml/code_4.js
+++ algoexperthtml/code_4.js
@@ -4,10 +4,43 @@ function getNthFib(n) {
     while (counter <= n) {
         let nextFib = lastTwo[0] + lastTwo[1];
         lastTwo[0] = lastTwo[1];
-        lastTwo[1] = nextFib; 
+        lastTwo[1] = nextFib;
 
         counter++;
     }
     return n > 1 ? lastTwo[1] : lastTwo[0];
 
+}
+
+
+
+
+function findThreeLargestNumbers(array) {
+    const highestThree = [null, null, null];
+    for (const num of array) {
+        updatedLargest(highestThree, num);
+    }
+    return highestThree;
+}
+
+function updatedLargest(threeLargest, num) {
+
+    if (threeLargest[2] === null || threeLargest[2] < num) {
+        shiftAndUpdate(threeLargest, num, 2);
+    } else if (threeLargest[1] === null || threeLargest[1] < num) {
+        shiftAndUpdate(threeLargest, num, 1);
+    } else if (threeLargest[0] === null || threeLargest[0] < num) {
+        shiftAndUpdate(threeLargest, num, 0);
+    }
+
+}
+
+function shiftAndUpdate(array, num, idx) {
+    for (let i = 0; i <= idx; i++) {
+        if (i == idx) {
+            array[i] = num;
+        } else {
+            array[i] = array[i + 1];
+        }
+    }
 }
