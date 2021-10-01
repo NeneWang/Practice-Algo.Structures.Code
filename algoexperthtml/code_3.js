d0620c4c6ed8c2741e1fa71d9f1025739adfb6be
NelsonWang <wangnelson2@gmail.com>
2021-10-01 09:53:55 -0400

    algo | findThreeLargest | 3

--- algoexperthtml/code_3.js
+++ algoexperthtml/code_3.js
@@ -162,3 +162,35 @@ function binarySearchHelper2(array, target, left, right) {
     }
     return -1;
 }
+
+
+function findThreeLargestNumbers(array) {
+    // Write your code here.
+    const threeLargest = [null, null, null];
+    for (const num of array) {
+        updatedLargest(threeLargest, num);
+    }
+    return threeLargest;
+}
+
+function updateLargest(threeLargest, num) {
+    if (threeLargest[2] === null || num > threeLargest[2]) {
+        shiftAndUpdate(array, num, 2);
+    } else if (threeLargest[1] === null || num > threeLargest[1]) {
+        shiftAndUpdate(array, num, 1);
+    } else if (threeLargest[0] === null || num > threeLargest[0]) {
+        shiftAndUpdate(array, num, 0);
+    }
+
+
+}
+
+function shiftAndUpdate(array, num, idx) {
+    for (let i = 0; i <= idx; i++) {
+        if (i == idx) {
+            array[i] = num;
+        } else {
+            array[i] = array[i + 1];
+        }
+    }
+}
