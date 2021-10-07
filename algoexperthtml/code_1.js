c6054ad66ede7f96d71e87c7883eeb662f5e43b3
NelsonWang <wangnelson2@gmail.com>
2021-10-07 18:27:15 -0400

    bubble sort | bubblesort

--- algoexperthtml/code_1.js
+++ algoexperthtml/code_1.js
@@ -354,3 +354,29 @@ function shiftAndUpdate(array, num, idx) {
         }
     }
 }
+
+
+function bubbleSort(array) {
+    let isSorted = false;
+    let counter = 0;
+    while (!isSorted) {
+        isSorted = true;
+        for (let i = 0; i < array.length - 1 - counter; i++) {
+            if (array[i] > array[i + 1]) {
+                swap(i, i + 1, array);
+                isSorted = false;
+            }
+
+
+        }
+
+        counter++;
+    }
+    return array;
+}
+
+function swap(i, j, array) {
+    const temp = array[j];
+    array[j] = array[i];
+    array[i] = temp;
+}
