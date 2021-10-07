c9a56d65eff4ec0ce23fda8fa74285ff54f1ada3
NelsonWang <wangnelson2@gmail.com>
2021-10-07 18:18:29 -0400

    bubble sort

--- algoexperthtml/code_2.js
+++ algoexperthtml/code_2.js
@@ -181,11 +181,25 @@ function shiftAndUpdate(array, num, idx) {
 
 
 function bubbleSort(array) {
-    // Write your code here.
+    // Write your code here.\
+    let isSorted = false;
+    let counter = 0;
+    while (!isSorted) {
+        isSorted = true;
+        for (let i = 0; i < array.length - 1 - counter; i++) {
+            if (array[i] > array[i + 1]) {
+                swap(i, i + 1, array);
+                isSorted = false;
+            }
+        }
+        counter++
+    }
+    return array;
+
 }
 
 function swap(i, j, array) {
-    const tempt = array[j];
+    const temp = array[j];
     array[j] = array[i];
     array[i] = temp;
 }
