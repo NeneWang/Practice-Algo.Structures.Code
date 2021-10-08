6c656f980ae548a5ce95065b98b0b16fffd8b98f
NelsonWang <wangnelson2@gmail.com>
2021-10-08 09:14:59 -0400

    algo | sort | insertion

--- algoexperthtml/code_1.js
+++ algoexperthtml/code_1.js
@@ -380,3 +380,14 @@ function swap(i, j, array) {
     array[j] = array[i];
     array[i] = temp;
 }
+
+function insertionSort(array) {
+    for (let i = 1; i < array.length; i++) {
+        let j = i;
+        while (j > 0 && array[j] < array[j - 1]) {
+            swap(j, j - 1, array);
+            j -= 1;
+        }
+    }
+    return array;
+}
