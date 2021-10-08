9ea52f0945650d620c578c961ecb3fab31ea4a62
NelsonWang <wangnelson2@gmail.com>
2021-10-08 09:06:52 -0400

    bubble sort | s2

--- algoexperthtml/code_3.js
+++ algoexperthtml/code_3.js
@@ -172,3 +172,27 @@ function updatedLargest(threeLargest, num) {
 
 function shiftAndUpdate(array, num, idx) {
 }
+
+
+function bubblesort(array){
+    let count = 0;
+    let isSorted = false;
+    while(!isSorted){
+        isSorted = true;
+        for(let i=0; i<array.length-1-count; i++){
+            if(array[i] > array[i+1]){
+                swap(i, j, array);
+                isSorted = false;
+            }
+        }
+        count++;
+    }
+
+    return array;
+}
+
+function swap(i, j, array){
+    const temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+}
