6f5d8fe550a1225c18c611f058077a96c39df53d
NelsonWang <wangnelson2@gmail.com>
2021-10-08 10:01:57 -0400

    algo | palindrome 3

--- algoexperthtml/code_3.js
+++ algoexperthtml/code_3.js
@@ -221,3 +221,19 @@ function insertionSort(array) {
     }
     return array;
 }
+
+function isPalindrom(string) {
+    let leftIdx = 0;
+    let rightIdx = string.length - 1;
+
+    while (leftIdx < rightIdx) {
+        if (string[leftIdx] != string[rightIdx]) {
+            return false;
+        }
+
+        leftIdx++;
+        rightIdx--;
+    }
+    return true;
+
+}
