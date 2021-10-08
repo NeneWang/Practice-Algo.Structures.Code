06e3d9946b5e2b14d1d6cd8f0d1a4856ba374db8
NelsonWang <wangnelson2@gmail.com>
2021-10-08 09:58:47 -0400

    algo | palindrome 2

--- algoexperthtml/code_2.js
+++ algoexperthtml/code_2.js
@@ -228,3 +228,11 @@ function selectionSort(array) {
     }
     return array;
 }
+
+function isPalindrome(string) {
+    let reverse = "";
+    for (let i = string.length - 1; i >= 0; i--) {
+        reverse += string[i];
+    }
+    return reverse === string;
+}
