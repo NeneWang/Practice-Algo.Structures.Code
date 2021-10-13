edacb670dec3b9afb58e506bcfc8bbe65f42d500
NelsonWang <wangnelson2@gmail.com>
2021-10-13 15:24:21 -0400

    algo | encoding 1

--- algoexperthtml/code_1.js
+++ algoexperthtml/code_1.js
@@ -437,5 +437,31 @@ function getNewLetter2(letter, key) {
 function getNewLetter2(letter, key) {
     const newLetterKey = letter.charCodeAt() + key;
     return newLetterKey <= 122 ? String.fromCharCode(newLetterKey) : String.fromCharCode(96 + newLetterKey % 122);
+}
+
+function runLengthEncoding(string) {
+
+    const encodedStringCharacters = [];
+    let currentRunLength = 1;
+
+    for (let i = 1; i < string.length; i++) {
+
+        const currentCharacter = string[i];
+        const previousCharacter = string[i-1];
+
+        if (currentCharacter !== previousCharacter || currentRunLength === 9) {
+            encodedStringCharacters.push(currentRunLength.toString());
+            encodedStringCharacters.push(previousCharacter);
+            currentRunLength = 0;
+        }
+
+        currentRunLength++;
+    }
+
+    encodedStringCharacters.push(currentRunLength.toString());
+    encodedStringCharacters.push(string[string.length - 1]);
+    return encodedStringCharacters.join('');
+
+
 
 }
