cfc3eb0f5903001634323698d208faf42a8aac5a
NelsonWang <wangnelson2@gmail.com>
2021-10-17 15:31:18 -0400

    algo | intermediate | monotomic array | breaks2

--- algoexperthtml/code_3.js
+++ algoexperthtml/code_3.js
@@ -274,3 +274,30 @@ function smallestDifference(arrayOne, arrayTwo) {
 
     return smallestPair;
 }
+
+
+function isMonotonic(array) {
+
+    // If the length oth the array is minus two then tere doesn't make sense to ave the direction
+    if (array.length <= 2) return true;
+
+    // Get the direction based on what's next
+    let direction = array[1] - array[0];
+    for (let i = 2; i < array.length; i++) {
+        if (direction == 0) {
+            direction = array[i] - array[i-1];
+            continue;
+        }
+        if(breaksDirection(direction, array[i-1], array[i])){
+            return false;
+        }
+    }
+    return true;
+}
+
+
+function breaksDirection(direction, previousInt, currentInt) {
+    const difference = currentInt - previousInt;
+    if (direction > 0) return difference < 0;
+    return difference > 0;
+}
