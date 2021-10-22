d3a63bbab37f738d8b94f00cf2e18b99eb3e52b8
NelsonWang <wangnelson2@gmail.com>
2021-10-22 13:31:39 -0400

    algo | intermediate | intervals 2

--- algoexperthtml/code_2.js
+++ algoexperthtml/code_2.js
@@ -504,3 +504,25 @@ function firstDuplicateValue(array) {
 
     return -1;
 }
+
+
+function mergeOverlappingIntervals(intervals) {
+    const sortedIntervals = intervals.sort((a, b) => a[0] - b[0]);
+
+    const mergedIntervals = [];
+    let currentInterval = sortedIntervals[0];
+    mergedIntervals.push(currentInterval);
+
+    for (const nextInterval of sortedIntervals) {
+        const [_, currentIntervalEnd] = currentInterval;
+        const [nextIntervalStart, nextIntervalEnd] = nextInterval;
+
+        if(currentIntervalEnd >= nextIntervalStart) currentInterval[1] = Math.max(currentIntervalEnd, nextIntervalEnd)
+        else{
+            currentInterval = nextInterval;
+            mergedIntervals.push(currentInterval);
+        }
+        
+    }
+    return mergedIntervals;
+}
