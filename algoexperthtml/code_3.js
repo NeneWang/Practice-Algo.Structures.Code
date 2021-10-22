52265775fd6b3ba0104392a126c127fc554f2a43
NelsonWang <wangnelson2@gmail.com>
2021-10-22 13:36:00 -0400

    algo | intermediate | intervals 3

--- algoexperthtml/code_3.js
+++ algoexperthtml/code_3.js
@@ -356,3 +356,24 @@ function arrayOfProducts(array) {
     return products;
 
 }
+
+
+function mergeOverlappingIntervals(intervals) {
+    const sortedIntervals = intervals.sort((a, b) => a[0] - b[0]);
+
+    const mergedIntervals = [];
+    let currentInterval = sortedIntervals[0];
+    mergedIntervals.push(currentInterval);
+
+    for (const nextInterval of sortedIntervals) {
+        const [_, currentIntervalEnd] = currentInterval;
+        const [nextIntervalStart, nextIntervalEnd] = nextInterval;
+
+        if(currentIntervalEnd >= nextIntervalStart) currentInterval[1] = Math.max(currentIntervalEnd, nextIntervalEnd)
+        else{
+            currentInterval = nextInterval;
+            mergedIntervals.push(currentInterval);
+        }
+    }
+    return mergedIntervals;
+}
