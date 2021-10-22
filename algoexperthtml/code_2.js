f2fe5a93abbcff371c9ed1d19278fa3aff165efe
NelsonWang <wangnelson2@gmail.com>
2021-10-22 15:07:53 -0400

    algo | intermediate | BST 2 | insert

--- algoexperthtml/code_2.js
+++ algoexperthtml/code_2.js
@@ -498,8 +498,8 @@ function firstDuplicateValue(array) {
     // Write your code here.
     for (let i = 0; i < array.length; i++) {
         const absValue = Math.abs(array[i]);
-        if(array[absValue -1] <0) return absValue;
-        array[absValue - 1 ] *= -1;
+        if (array[absValue - 1] < 0) return absValue;
+        array[absValue - 1] *= -1;
     }
 
     return -1;
@@ -517,12 +517,103 @@ function mergeOverlappingIntervals(intervals) {
         const [_, currentIntervalEnd] = currentInterval;
         const [nextIntervalStart, nextIntervalEnd] = nextInterval;
 
-        if(currentIntervalEnd >= nextIntervalStart) currentInterval[1] = Math.max(currentIntervalEnd, nextIntervalEnd)
-        else{
+        if (currentIntervalEnd >= nextIntervalStart) currentInterval[1] = Math.max(currentIntervalEnd, nextIntervalEnd)
+        else {
             currentInterval = nextInterval;
             mergedIntervals.push(currentInterval);
         }
-        
+
     }
     return mergedIntervals;
+}
+
+
+class BST {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+
+    insert(value) {
+        if (value < this.value) {
+            if (this.left === null) {
+                this.left = new BST(value);
+            } else {
+                this.left.insert(value);
+            }
+        } else {
+            if (this.right === null) {
+                this.right = new BST(value);
+            } else {
+                this.right.insert(value);
+            }
+        }
+
+        return this;
+    }
+
+    contains(value) {
+        // Write your code here.
+        if (value < this.value) {
+            if (this.left === null) {
+                return false;
+            } else {
+                return this.left.contains(value);
+            }
+        } else if (value > this.value) {
+            if (this.right === null) {
+                return false;
+            } else {
+                return this.right.contains(value);
+            }
+        } else {
+            return (true);
+        }
+    }
+
+    remove(value, parent = null) {
+        // Write your code here.
+        // Do not edit the return statement of this method.
+        if (value < this.value) {
+            if (this.left !== null) {
+                this.left.remove(value, this);
+            }
+        } else if (value > this.value) {
+            if (this.right !== null) {
+                this.right.remove(value, this);
+            }
+        } else {
+            if (this.left !== null && this.right !== null) {
+                this.value = this.right.getMinValue();
+                this.right.remove(this.value, this);
+            } else if (parent === null) {
+                if (this.left !== null) {
+                    this.value = this.left.value;
+                    this.right = this.left.right;
+                    this.left = this.left.left;
+                } else if (this.right !== null) {
+                    this.value = this.right.value;
+                    this.left = this.right.left;
+                    this.right = this.right.right;
+                } else {
+
+                }
+            } else if (parent.left === this) {
+                parent.left = this.left !== null ? this.left : this.right;
+            } else if (parent.right === this) {
+                parent.right = this.left !== null ? this.left : this.right;
+            }
+
+        }
+        return this;
+    }
+
+    getMinValue() {
+        if (this.left == null) {
+            return this.value;
+        } else {
+            return this.left.getMinValue();
+        }
+    }
 }
