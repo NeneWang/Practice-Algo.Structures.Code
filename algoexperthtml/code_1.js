860e1393bfd67743f3820c495616aa19e89fc5c1
NelsonWang <wangnelson2@gmail.com>
2021-10-22 14:57:22 -0400

    algo | intermediate | BST 2 | remove

--- algoexperthtml/code_1.js
+++ algoexperthtml/code_1.js
@@ -787,9 +787,48 @@ class BST {
         }
     }
 
-    remove(value) {
+    remove(value, parent = null) {
         // Write your code here.
         // Do not edit the return statement of this method.
+        if (value < this.value) {
+            if (this.left !== null) {
+                this.left.remove(value, this);
+            }
+        } else if (value > this.value) {
+            if (this.right !== null) {
+                this.right.remove(value, this);
+            }
+        } else {
+            if (this.left !== null && this.right !== null) {
+                this.value = this.right.getMinValue();
+                this.right.remove(this.value, this);
+            } else if (parent === null) {
+                if (this.left !== null) {
+                    this.value = this.left.value;
+                    this.right = this.left.right;
+                    this.left = this.left.left;
+                } else if (this.right !== null) {
+                    this.value = this.right.value;
+                    this.left = this.right.left;
+                    this.right = this.right.right;
+                } else {
+
+                }
+            } else if (parent.left === this) {
+                parent.left = this.left !== null ? this.left : this.right;
+            } else if (parent.right === this) {
+                parent.right = this.left !== null ? this.left : this.right;
+            }
+
+        }
         return this;
+    }
+
+    getMinValue() {
+        if (this.left == null) {
+            return this.value;
+        } else {
+            return this.left.getMinValue();
+        }
     }
 }
