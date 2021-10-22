7cb0e7e39c1b8dc61e731b943c2e692447408972
NelsonWang <wangnelson2@gmail.com>
2021-10-22 18:15:49 -0400

    algo | intermediate | validate BST 2

--- algoexperthtml/code_2.js
+++ algoexperthtml/code_2.js
@@ -616,3 +616,26 @@ class BST {
         }
     }
 }
+
+
+// This is an input class. Do not edit.
+class BST {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+function validateBst(tree) {
+    // Write your code here.
+    return validateBstHelper(tree, -Infinity, Infinity);
+}
+
+
+function validateBstHelper(tree, minValue, maxValue) {
+    if(tree === null) return true;
+    if(tree.value < minValue || tree.value >= maxValue) return false;
+    const isLeftValid = validateBstHelper(tree.left, minValue, tree.value);
+    return isLeftValid && validateBstHelper(tree.right, tree.value, maxValue);
+}
