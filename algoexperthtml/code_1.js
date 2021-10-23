7b853a5b243accc36ca17180f89abea8dc727307
NelsonWang <wangnelson2@gmail.com>
2021-10-23 17:29:49 -0400

    algo | intermediate | reconstruct bst

--- algoexperthtml/code_1.js
+++ algoexperthtml/code_1.js
@@ -950,9 +950,30 @@ function findKthLargestValueInBst(tree, k) {
     return sortedNodeValues[sortedNodeValues.length - k];
 }
 
-function inOrderTraverse(node, sortedTreeValues){
-    if(node === null) return;
+function inOrderTraverse(node, sortedTreeValues) {
+    if (node === null) return;
     inOrderTraverse(node.left, sortedTreeValues);
     sortedTreeValues.push(node.value);
     inOrderTraverse(node.right, sortedTreeValues);
+
+}
+
+
+function reconstructBst(preOrderTraversalValues) {
+
+    if (preOrderTraversalValues.length === 0) return null;
+
+    const currentValue = preOrderTraversalValues[0];
+    let rightSubtreeRootIdx = preOrderTraversalValues.length;
+
+    for (let idx = 1; idx < preOrderTraversalValues.length; idx++) {
+        const value = preOrderTraversalValues[idx];
+        if (value >= currentValue) {
+            rightSubtreeRootIdx = idx;
+            break;
+        }
+    }
+    const leftSubtree = reconstructBst(preOrderTraversalValues.slice(1, rightSubtreeRootIdx));
+    const rightSubtree = reconstructBst(preOrderTraversalValues.slice(rightSubtreeRootIdx));
+    return new BST(currentValue, leftSubtree, rightSubtree);
 }
