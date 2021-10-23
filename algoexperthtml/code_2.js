7dd03a3f5b6346d11a62e9d6d1013e98a3507869
NelsonWang <wangnelson2@gmail.com>
2021-10-23 17:15:08 -0400

    algo | intermediate | min height BST build 2

--- algoexperthtml/code_2.js
+++ algoexperthtml/code_2.js
@@ -634,8 +634,58 @@ function validateBst(tree) {
 
 
 function validateBstHelper(tree, minValue, maxValue) {
-    if(tree === null) return true;
-    if(tree.value < minValue || tree.value >= maxValue) return false;
+    if (tree === null) return true;
+    if (tree.value < minValue || tree.value >= maxValue) return false;
     const isLeftValid = validateBstHelper(tree.left, minValue, tree.value);
     return isLeftValid && validateBstHelper(tree.right, tree.value, maxValue);
+}
+
+
+// --------------------------------------------------- MIN HEIGHT BST -------------------------------------------------------------------
+
+function minHeightBst(array) {
+    // Write your code here.
+    return constructMinHeightBst(array, null, 0, array.length - 1);
+}
+
+function constructMinHeightBst(array, bst, startIdx, endIdx) {
+
+    if (startIdx > endIdx) {
+        return;
+    }
+    const midIdx = Math.floor((endIdx + startIdx) / 2);
+    const valueToAdd = array[midIdx];
+
+    if (bst === null) {
+        bst = new BST(valueToAdd);
+    } else {
+        bst.insert(valueToAdd);
+    }
+    constructMinHeightBst(array, bst, startIdx, midIdx - 1);
+    constructMinHeightBst(array, bst, midIdx + 1, endIdx);
+    return bst;
+}
+
+class BST {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+
+    insert(value) {
+        if (value < this.value) {
+            if (this.left === null) {
+                this.left = new BST(value);
+            } else {
+                this.left.insert(value);
+            }
+        } else {
+            if (this.right === null) {
+                this.right = new BST(value);
+            } else {
+                this.right.insert(value);
+            }
+        }
+    }
 }
