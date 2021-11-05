b2dd36b7b85bac863c628ddac947aef6eeea68d5
NelsonWang <wangnelson2@gmail.com>
2021-11-05 15:04:49 -0400

    algo | intermediate | find successor sol 2

--- algoexperthtml/code_1.js
+++ algoexperthtml/code_1.js
@@ -1140,3 +1140,30 @@ function getInOrderTraversalOrder(node, order = []) {
 
     return order;
 }
+
+
+function findSuccessor(tree, node) {
+    if (node.right !== null) return getLeftMostChild(node.right);
+    return getRightMostParent(node);
+}
+
+
+function getLeftMostChild(node){
+    let currentNode = node;
+    while(currentNode.left !== null){
+        currentNode = currentNode.left;
+    }
+
+    return currentNode;
+}
+
+function getRightMostParent(node){
+    let currentNode = node;
+    while(currentNode.parent !== null && currentNode.parent.right === currentNode){
+        currentNode = currentNode.parent;
+    }
+    return currentNode.parent;
+}
+
+
+
