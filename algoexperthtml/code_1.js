e39a0a7860aceec0b323fc434eeeba7187b9e3ab
NelsonWang <wangnelson2@gmail.com>
2021-11-08 17:26:05 -0500

    algo | inter | min amount of change | 1

--- algoexperthtml/code_1.js
+++ algoexperthtml/code_1.js
@@ -1189,3 +1189,16 @@ function numberOfWaysToMakeChange(n, denoms) {
     return ways[n];
 
 }
+
+function minNumberOfCoinsForChange(n, denoms) {
+    const numOfCoins = new Array(n + 1).fill(Infinity);
+    numOfCoins[0] = 0;
+    for (const denom of denoms) {
+        for (let amount = 0; amount < numOfCoins.length; amount++) {
+            if (denom <= amount) {
+                numOfCoins[amount] = Math.min(numOfCoins[amount], numOfCoins[amount - denom] + 1);
+            }
+        }
+    }
+    return numOfCoins[n] !== Infinity ? numOfCoins[n] : -1;
+}
