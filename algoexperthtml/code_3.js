319db94c1fd371f41a0ec688061c78cc3bce9893
NelsonWang <wangnelson2@gmail.com>
2021-11-07 21:29:12 -0500

    algo | inter | number of ways | 4

--- algoexperthtml/code_3.js
+++ algoexperthtml/code_3.js
@@ -435,6 +435,7 @@ function getTreeInfo(node) {
 function numberOfWaysToMakeChange(n, denoms) {
 
     const ways = Array[n + 1].fill(1);
+    ways[0] = 1;
     for (let denom of denoms) {
         for (let amount = 1; amount <= n; amount++) {
             if (denom <= amount) ways[amount] += ways[amount - denom];
--- algoexperthtml/code_4.js
+++ algoexperthtml/code_4.js
@@ -94,3 +94,14 @@ function mergeOverlappingIntervals(array) {
     }
     return mergedIntervals;
 }
+
+function numberOfWaysToMakeChange(n, denoms) {
+    const ways = new Array(n + 1).fill(0);
+    ways[0] = 1;
+    for (const denom of denoms) {
+        for (let amount = 1; amount <= n; amount++) {
+            if (denom <= amount) ways[amount] += ways[amount - denom];
+        }
+    }
+    return ways[n];
+}
