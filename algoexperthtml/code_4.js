f70aee9af5a61e528cbbf5db2d944dd223c038ae
NelsonWang <wangnelson2@gmail.com>
2021-11-10 17:29:42 -0500

    algo | inter | breath first search | 5

--- algoexperthtml/code_4.js
+++ algoexperthtml/code_4.js
@@ -123,3 +123,30 @@ function factorial(num) {
     }
     return result;
 }
+
+class Node {
+    constructor(name) {
+      this.name = name;
+      this.children = [];
+    }
+  
+    addChild(name) {
+      this.children.push(new Node(name));
+      return this;
+    }
+  
+    breadthFirstSearch(array) {
+      // Write your code here.
+        const queue = [this]
+        while(queue.length > 0){
+            const current = queue.shift();
+            array.push(current.name);
+            for(const children of current.children){
+                queue.push(children);
+            }
+        }
+        return array;
+
+    }
+  }
+  
