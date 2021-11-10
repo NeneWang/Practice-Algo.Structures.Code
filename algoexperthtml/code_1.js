2f68a828958574156ca63716184acaac20137d63
NelsonWang <wangnelson2@gmail.com>
2021-11-10 14:09:04 -0500

    algo | inter | hassinglecycle

--- algoexperthtml/code_1.js
+++ algoexperthtml/code_1.js
@@ -1244,3 +1244,31 @@ function kadanesAlgorithm(array) {
     }
     return maxSoFar;
 }
+
+function hasSingleCycle(array) {
+    let numElementVisited = 0;
+    let currentIdx = 0;
+}
+
+function hasSingleCycle(array) {
+    let numElementVisited = 0;
+    let currentIdx = 0;
+    while (numElementVisited < array.length) {
+        if (numElementVisited > 0 && currentIdx === 0) return false;
+        numElementVisited++;
+        currentIdx = getNextIdx(currentIdx, array)
+    }
+    return currentIdx === 0;
+}
+
+
+function getNextIdx(currentIdx, array) {
+    const jump = array[currentIdx];
+    const nextIdx = (currentIdx + jump) % array.length;
+    return nextIdx >= 0 ? nextIdx : nextIdx + array.length;
+}
+
+
+
+
+
