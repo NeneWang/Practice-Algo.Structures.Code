fa189db1d9c7481c2c171cd774e812f8a79fffb8
NelsonWang <wangnelson2@gmail.com>
2021-11-09 22:21:08 -0500

    algo | inter | levelheindistance | 2

--- algoexperthtml/code_3.js
+++ algoexperthtml/code_3.js
@@ -473,7 +473,7 @@ function levenshteinDistance(str1, str2) {
             if (str2[i - 1] === str1[j - 1]) {
                 edits[i][j] = edits[i - 1][j - 1];
             } else {
-                edits[i][j] = 1 + Math.min(edits[i - 1][j - 1], edits[i - 1][j], edits[i][j - 1]);
+                edits[i][j]=1 + min(edits[i][j-1],edits[i-1][j],edits[i-1][j-1]);
             }
         }
     }
