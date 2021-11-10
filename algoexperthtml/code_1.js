e220f5b4838a10e948a8c7b8674796462a07ae50
NelsonWang <wangnelson2@gmail.com>
2021-11-10 12:23:55 -0500

    algo | inter | kadanesAlgorithm | 1

--- algoexperthtml/code_1.js
+++ algoexperthtml/code_1.js
@@ -1240,8 +1240,7 @@ function kadanesAlgorithm(array) {
     for (let i = 1; i < array.length; i++) {
         const num = array[i];
         maxEndingHere = Math.max(num, maxEndingHere + num);
-        maxSoFar = Math.max(maxSoFar, maxEndingHere);
+        maxSoFar = Math.max(maxEndingHere, maxSoFar);
     }
     return maxSoFar;
-
 }
