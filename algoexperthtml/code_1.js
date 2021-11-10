d982af845aa137bdd9cd9696e8d1e6f34c7bf610
NelsonWang <wangnelson2@gmail.com>
2021-11-09 23:19:52 -0500

    algo | inter | number of ways to traverse | 2

--- algoexperthtml/code_1.js
+++ algoexperthtml/code_1.js
@@ -1227,3 +1227,8 @@ function levenshteinDistance(str1, str2) {
     return edits[str2.length][str1.length];
 
 }
+
+function numberOfWaysToTraverseGraph(width, height) {
+    if (width === 1 || height === 1) return 1;
+    return numberOfWaysToTraverseGraph(width - 1, height) + numberOfWaysToTraverseGraph(width, height - 1);
+}
--- algoexperthtml/code_2.js
+++ algoexperthtml/code_2.js
@@ -825,10 +825,34 @@ function levenshteinDistance(str1, str2) {
             if (str2[i - 1] === str1[j - 1]) {
                 edits[i][j] = edits[i - 1][j - 1];
             } else {
-                edits[i][j]=1 + Math.min(edits[i][j-1],edits[i-1][j],edits[i-1][j-1]);
+                edits[i][j] = 1 + Math.min(edits[i][j - 1], edits[i - 1][j], edits[i - 1][j - 1]);
             }
         }
     }
     return edits[str2.length][str1.length];
 
+}
+
+function numberOfWaysToTraverseGraph(width, height) {
+    const numberOfWays = [];
+    for (let i = 0; i < height + 1; i++) {
+        numberOfWays.push([]);
+        for (let j = 0; j < width + 1; j++) {
+            numberOfWays[i].push(0);
+        }
+    }
+
+    for (let widthIdx = 1; widthIdx < width + 1; widthIdx++) {
+        for (let heightIdx = 1; heightIdx < height + 1; heightIdx++) {
+            if (widthIdx === 1 || heightIdx === 1) {
+                numberOfWays[heightIdx][widthIdx] = 1;
+            } else {
+                const waysLeft = numberOfWays[heightIdx][widthIdx - 1];
+                const waysUp = numberOfWays[heightIdx - 1][widthIdx];
+                numberOfWays[heightIdx][widthIdx] = waysLeft + waysUp;
+            }
+        }
+    }
+
+    return numberOfWays[height][width];
 }
