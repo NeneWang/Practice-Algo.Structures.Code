f4d57dbb6b6a43a292a8c6b0d9672840d06b83e8
NelsonWang <wangnelson2@gmail.com>
2021-11-10 17:05:52 -0500

    algo | inter | breath first search

--- algoexperthtml/code_1.js
+++ algoexperthtml/code_1.js
@@ -1270,5 +1270,26 @@ function getNextIdx(currentIdx, array) {
 
 
 
+class Node {
+    constructor(name) {
+        this.name = name;
+        this.children = [];
+    }
 
+    addChild(name) {
+        this.children.push(new Node(name));
+        return this;
+    }
 
+    breadthFirstSearch(array) {
+        const queue = [this];
+        while (queue.length > 0) {
+            const current = queue.shift();
+            array.push(current.name);
+            for (const child of current.children) {
+                queue.push(child);
+            }
+        }
+        return array;
+    }
+}
