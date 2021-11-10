e0c362f85152628db6cb546e951436372b7f24c6
NelsonWang <wangnelson2@gmail.com>
2021-11-09 21:37:13 -0500

    algo | inter | levelheindistance

--- algoexperthtml/code_3.js
+++ algoexperthtml/code_3.js
@@ -443,3 +443,41 @@ function numberOfWaysToMakeChange(n, denoms) {
     }
     return ways[n];
 }
+
+
+function minNumberOfCoinsForChange(n, denoms) {
+    const numOfCoins = new Array(n + 1).fill(Infinity);
+    numOfCoins[0] = 0;
+    for (const denom of denoms) {
+        for (let amount = 0; amount <= n; amount++) {
+            numOfCoins[amount] = Math.min(numOfCoins[amount], numOfCoins[amount - denom] + 1);
+        }
+    }
+    return numOfCoins[n] !== Infinity ? numOfCoins[n] : -1;
+}
+
+function levelsheinDistance(str1, str2) {
+    const edits = [];
+    for (let i = 0; i < str2.length + 1; i++) {
+        const row = [];
+        for (let j = 0; j < str1.length + 1; j++) {
+            row.push(j);
+        }
+
+        row[0] = i;
+        edits.push(row);
+    }
+
+    for (let i = 1; i < str2.length + 1; i++) {
+        for (let j = 1; j < str1.length + 1; j++) {
+            if (str2[i - 1] === str1[j - 1]) {
+                edits[i][j] = edits[i - 1][j - 1];
+            } else {
+                edits[i][j] = 1 + Math.min(edits[i - 1][j - 1], edits[i - 1][j], edits[i][j - 1]);
+            }
+        }
+    }
+    return edits[str2.length][str1.length];
+
+
+}
