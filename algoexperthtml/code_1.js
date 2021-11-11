ca4acd46adc3e138bc7bee2e91e18458e3ce1713
NelsonWang <wangnelson2@gmail.com>
2021-11-10 19:04:25 -0500

    algo | inter | youngest common ancestor | 1

--- algoexperthtml/code_1.js
+++ algoexperthtml/code_1.js
@@ -1338,3 +1338,46 @@ function getUnvisitedNeighbors(i, j, matrix, visited) {
     if (j < matrix[0].length - 1 && !visited[i][j + 1]) unvisitedNeighbors.push([i, j + 1]);
     return unvisitedNeighbors;
 }
+
+
+// This is an input class. Do not edit.
+class AncestralTree {
+    constructor(name) {
+        this.name = name;
+        this.ancestor = null;
+    }
+}
+
+function getYoungestCommonAncestor(topAncestor, descendantOne, descendantTwo) {
+    // Write your code here.
+    const depthOne = getDescendantDepth(descendantOne, topAncestor);
+    const depthTwo = getDescendantDepth(descendantTwo, topAncestor);
+    if (depthOne > depthTwo) {
+        return backtrackAncestralTree(descendantOne, descendantTwo, depthOne - depthTwo);
+    } else {
+        return backtrackAncestralTree(descendantTwo, descendantOne, depthTwo - depthOne);
+    }
+}
+
+function getDescendantDepth(descendant, topAncestor) {
+    let depth = 0;
+    while (descendant !== topAncestor) {
+        depth++;
+        descendant = descendant.ancestor;
+    }
+
+    return depth;
+}
+
+function backtrackAncestralTree(lowerDescendant, higherDescendant, diff) {
+    while (diff > 0) {
+        lowerDescendant = lowerDescendant.ancestor;
+        diff--;
+    }
+
+    while (lowerDescendant !== lowerDescendant.ancestor) {
+        lowerDescendant = lowerDescendant.ancestor;
+        higherDescendant = higherDescendant.ancestor;
+    }
+    return lowerDescendant;
+}
--- algoexperthtml/code_2.js
+++ algoexperthtml/code_2.js
@@ -925,7 +925,7 @@ function riverSizes(matrix) {
     const visited = matrix.map(row => row.map(value => false));
     for (let i = 0; i < matrix.length; i++) {
         for (let j = 0; j < matrix[i].length; j++) {
-            if (visited[i][j]) continue;
+            if(visited[i][j]) continue;
             traverseNode(i, j, matrix, visited, sizes);
         }
     }
