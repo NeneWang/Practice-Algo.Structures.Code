2b0d61c2be22491ee0a23ba38bd99f9f0884cf8c
NelsonWang <wangnelson2@gmail.com>
2021-11-13 11:12:55 -0500

    algo | inter | min heap | insert before

--- algoexperthtml/code_1.js
+++ algoexperthtml/code_1.js
@@ -1751,3 +1751,80 @@ class MinHeap {
         heap[i] = temp;
     }
 }
+
+// ______________ LINKED LIST CONSTRUCTION ________________
+
+// This is an input class. Do not edit.
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.prev = null;
+        this.next = null;
+    }
+}
+
+// Feel free to add new properties and methods to the class.
+class DoublyLinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    setHead(node) {
+        if (this.head === null) {
+            this.head = node;
+            this.tail = node;
+            return;
+        }
+        this.insertBefore(this.head, node)
+    }
+
+    setTail(node) {
+        if (this.tail === null) {
+            this.setHead(node);
+            return;
+        }
+        this.insertAfter(this.tail, node);
+    }
+
+    insertBefore(node, nodeToInsert) {
+
+        // My guess is that the following would be a node case.
+        if (nodeToInsert === this.head && nodeToInsert === this.tail) return;
+        this.remove(nodeToInsert);
+        nodeToInsert.prev = node.prev;
+        nodeToStart.next = node;
+
+        if (node.prev === null){
+            this.head = nodeToInsert;
+        }else{
+            node.prev.next = nodeToInsert;
+        }
+
+        node.prev = nodeToInsert;
+    }
+
+    insertAfter(node, nodeToInsert) {
+        // Write your code here.
+    }
+
+    insertAtPosition(position, nodeToInsert) {
+        // Write your code here.
+    }
+
+    removeNodesWithValue(value) {
+        // Write your code here.
+    }
+
+    remove(node) {
+        // Write your code here.
+    }
+
+    containsNodeWithValue(value) {
+        // Write your code here.
+    }
+}
+
+// Do not edit the lines below.
+exports.Node = Node;
+exports.DoublyLinkedList = DoublyLinkedList;
