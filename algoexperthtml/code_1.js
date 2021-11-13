8dcf17718d8300e68d5a652c3102cf3a2520ecf9
NelsonWang <wangnelson2@gmail.com>
2021-11-13 16:25:32 -0500

    algo | inter | get permutation |3

--- algoexperthtml/code_1.js
+++ algoexperthtml/code_1.js
@@ -1977,9 +1977,13 @@ function permutationHelper(array, currentPermutation, permutations) {
         permutations.push(currentPermutation);
     } else {
         for (let i = 0; i < array.length; i++) {
-            const newArray = array.slice(0, i).concat(array.slice(i + 1));
-            const newPermutation = currentPermutation.concat(array[i]);
-            permutationHelper(newArray, newPermutation, permutations);
+
+            // Why the slice thought? if its based on the array lenght, and concatinging to the array based on i and + 1, which is removing one of thme of I get it, so the array slice tets covere each time
+            const newArray = array.slice(0, i).concat(array.slice(i + 1))
+            const newPermutation = permutations.concat(array[i]);
+            permutationHelper(newArray, newPermutation, permutations)
+
+
         }
     }
 }
