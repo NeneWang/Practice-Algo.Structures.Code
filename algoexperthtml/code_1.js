d7c6a9fc4b143e85a892cb5b676ec5c5b5a76b55
NelsonWang <wangnelson2@gmail.com>
2021-11-18 09:09:20 -0500

    algo | inter | balancedbrackets

--- algoexperthtml/code_1.js
+++ algoexperthtml/code_1.js
@@ -2188,3 +2188,34 @@ class MinMaxStack {
         return this.minMaxStack[this.minMaxStack.length - 1].max;
     }
 }
+
+
+function balancedBrackets(string) {
+    const openingBrackets = '({[';
+    const closingBrackets = ')}]';
+    const matchingBrackets = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    }
+    const stack = [];
+
+    for (const char of string) {
+
+        if (openingBrackets.includes(char)) {
+            stack.push(char);
+        } else if (closingBrackets.includes(char)) {
+            if (stack.length <= 0) {
+                return false;
+            }
+
+            if (stack[stack.length - 1] == matchingBrackets[char]) {
+                stack.pop();
+            } else {
+                return false;
+            }
+        }
+    }
+    return stack.length === 0
+
+}
