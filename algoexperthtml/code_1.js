6091507c3cd0f7907b93eb841bb08b76b6c92d92
NelsonWang <wangnelson2@gmail.com>
2021-11-17 22:27:51 -0500

    algo | inter | searchSorted matrix | 1

--- algoexperthtml/code_1.js
+++ algoexperthtml/code_1.js
@@ -2068,3 +2068,18 @@ function numberOfWaysToTop(height, maxSteps) {
 
     return numberOfWays;
 }
+
+function searchInSortedMatrix(matrix, target) {
+    let row = 0;
+    let col = matrix[0].length - 1;
+    while (row < matrix.length && col >= 0) {
+        if (matrix[row][col] > target) {
+            col--;
+        } else if (matrix[row][col] < target) {
+            row++;
+        } else {
+            return[row, col];
+        }
+    }
+    return [-1, -1]
+}
