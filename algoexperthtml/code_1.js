5a0d26012207be864c789eab914c465366fe18d5
NelsonWang <wangnelson2@gmail.com>
2021-11-19 12:49:23 -0500

    algo | inter | sort stack

--- algoexperthtml/code_1.js
+++ algoexperthtml/code_1.js
@@ -2242,3 +2242,23 @@ function sunsetViews(buildings, direction) {
 
     return buildingWithSunsetViews;
 }
+
+function sortStack(stack) {
+
+    if (stack.length === 0) return stack;
+    const top = stack.pop();
+    sortStack(stack);
+    insertInSortedOrder(stack, top)
+    return stack;
+}
+
+function insertInSortedOrder(stack, value) {
+    if (stack.length === 0 | stack[stack.length - 1] <= value) {
+        stack.push(value);
+        return;
+    }
+    const top = stack.pop();
+    insertInSortedOrder(stack, value);
+    stack.push(top)
+
+}
