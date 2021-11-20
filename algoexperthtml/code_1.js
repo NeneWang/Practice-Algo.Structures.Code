80e02a2dda8947e593f975acfda377867099755d
NelsonWang <wangnelson2@gmail.com>
2021-11-20 12:22:32 -0500

    algo | inter | reverse words in string

--- algoexperthtml/code_1.js
+++ algoexperthtml/code_1.js
@@ -2349,3 +2349,35 @@ function isValidPart(string) {
     if (numericValue > 255) return false;
     return numericValue.toString().length == string.length;
 }
+
+
+function reverseWordsInString(string) {
+    const words = [];
+    let startOfWord = 0;
+    for (let idx = 0; idx < string.length; idx++) {
+        const character = string[idx];
+        if (character == ' ') {
+            words.push(string.slice(startOfWord, idx));
+            startOfWord = idx;
+        } else if (string[startOfWord] === ' ') {
+            words.push(' ');
+            startOfWord = idx;
+        }
+    }
+    words.push(string.slice(startOfWord));
+    reverseList(words);
+    return words.join('');
+}
+
+function reverseList(list) {
+    let start = 0,
+        end = list.length - 1;
+    while (start < end) {
+        const temporal = list[end];
+        list[end] = list[start];
+        list[start] = temporal;
+        start++;
+        end--;
+    }
+    return list;
+}
