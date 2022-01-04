ac021cd4578b56e4616b665889010bc96bb46b6b
NelsonWang <wangnelson2@gmail.com>
2022-01-04 05:44:46 -0500

    algo | d1 | 2

--- algoexperthtml/basic_2.js
+++ algoexperthtml/basic_2.js
@@ -1,37 +1,34 @@
 function twoNumberSum(array, targetSum) {
     // Write your code here.
-      array.sort((a,b) => a-b);
-      let left = 0;
-      let right = array.length-1;
-      while(left < right){
-          const sum = array[left]+array[right];
-          if(sum==targetSum){
-              return [array[left], array[right]];
-          }else if(sum<targetSum){
-              left++;
-          }else{
-              right--;
-          }
-      }
-      return [];
-  }
-  function isValidSubsequence(array, sequence) {
+    array.sort((a, b) => a - b);
+    let left = 0;
+    let right = array.length - 1;
+    while (left < right) {
+        const sum = array[left] + array[right];
+        if (sum == targetSum) {
+            return [array[left], array[right]];
+        } else if (sum < targetSum) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+    return [];
+}
+
+function isValidSubsequence(array, sequence) {
     // Write your code here.
-      let countSequence = 0;
-      
-      for(const value of array){
-          if(value == sequence[countSequence]){
-              countSequence++;
-          }
-      }
-      
-      if(countSequence==sequence.length){
-          return true;
-      }else{
-          return false;
-      }
-  }
-  
-  // Do not edit the line below.
-  
-  
+    let countSequence = 0;
+
+    for (const value of array) {
+        if (value == sequence[countSequence]) {
+            countSequence++;
+        }
+    }
+
+    if (countSequence == sequence.length) {
+        return true;
+    } else {
+        return false;
+    }
+}
