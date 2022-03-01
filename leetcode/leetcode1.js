eb27606bc1935a704d101eb2049c04e4f9acacf9
NelsonWang <wangnelson2@gmail.com>
2022-02-28 23:13:06 -0500

    cluimb stairs

--- leetcode/leetcode1.js
+++ leetcode/leetcode1.js
@@ -13,3 +13,25 @@ var maxProfit = function(prices) {
     }
     return max_profit;
 };
+
+// I guess another way to solv it is by asking how many took to solve the previous one? so for that to happen
+
+// memorize it on a map, and then figure out the previous one. and add the how many times and multiply them with the new ays?
+let memo = [];
+var climbStairs = function(n) {
+    memo = Array(n+1);
+    return climbHelp(n);
+}
+
+var climbHelp = function(n){
+    if(n<=2){
+        return n;
+    }    
+    else if(memo[n]){
+        return memo[n]
+    }
+    else{
+        memo[n] = climbHelp(n-1) + climbHelp(n-2)
+        return memo[n];
+    }
+}
